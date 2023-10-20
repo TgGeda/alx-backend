4da6ab9dae5362c321d456a0bcf7cdd9ab67b671
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-10-20 21:19:03 +0300

    0-simple_helper_function.py task completed

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,18 +7,25 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Takes 2 integer arguments and returns a tuple of size two
-    containing the start and end index corresponding to the range of
-    indexes to return in a list for those pagination parameters
-    Args:
-        page (int): page number to return (pages are 1-indexed)
-        page_size (int): number of items per page
-    Return:
-        tuple(start_index, end_index)
-    """
-    start, end = 0, 0
-    for i in range(page):
-        start = end
-        end += page_size
+    Calculates the start and end indices for a given page and page size in a paginated list.
 
-    return (start, end)
+    Args:
+        page (int): The page number to return (pages are 1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the start and end indices of the range.
+
+    """
+
+    # Calculate the start and end indices for the given page and page size
+    start_index = 0  # The start index of the range
+    end_index = 0  # The end index of the range
+
+    # Iterate over each page to calculate the indices
+    for current_page in range(page):
+        start_index = end_index  # Update the start index to be the previous end index
+        end_index += page_size  # Increment the end index by the page size to move the range
+
+    # Return the calculated start and end indices as a tuple
+    return (start_index, end_index)
