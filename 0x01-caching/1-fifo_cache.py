1f25b9eac41f1b809548aea2f22279299d16e267
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-10-24 23:34:57 +0300

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,54 +3,38 @@
 """
 from base_caching import BaseCaching
 
+
 class FIFOCache(BaseCaching):
     """
-    FIFOCache implements a First-In-First-Out (FIFO) caching system.
+    FIFOCache defines a FIFO caching system
     """
 
     def __init__(self):
         """
-        Initializes the FIFOCache class by calling the parent class's __init__ method.
-        It also initializes an empty list to keep track of the order of keys in the cache.
+        Initialize the class with the parent's init method
         """
         super().__init__()
-        self.key_order = []
+        self.order = []
 
     def put(self, key, item):
         """
-        Caches a key-value pair in the cache.
-
-        Args:
-            key: The key to cache.
-            item: The value associated with the key.
+        Cache a key-value pair
         """
         if key is None or item is None:
-            # If either key or item is None, do nothing.
-            return
+            pass
         else:
-            cache_size = len(self.cache_data)
-            if cache_size >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                # If the cache is full and the new key is not already in the cache, remove the oldest item.
-                oldest_key = self.key_order[0]
-                print("DISCARD: {}".format(oldest_key))
-                del self.cache_data[oldest_key]
-                del self.key_order[0]
-            self.key_order.append(key)
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.order[0]))
+                del self.cache_data[self.order[0]]
+                del self.order[0]
+            self.order.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
         """
-        Retrieves the value associated with a given key from the cache.
-
-        Args:
-            key: The key to retrieve the value for.
-
-        Returns:
-            The value associated with the key, or None if the key is None or doesn't exist in the cache.
+        Return the value linked to a given key, or None
         """
-        if key is not None and key in self.cache_data:
-            # If the key exists in the cache_data dictionary, return the corresponding value.
+        if key is not None and key in self.cache_data.keys():
             return self.cache_data[key]
-        else:
-            # If the key is None or doesn't exist in the cache, return None.
-            return None
+        return None
