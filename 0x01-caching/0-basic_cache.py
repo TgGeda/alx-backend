d4b5770d742c23f9ac8497366534a379f176ce84
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-10-24 20:52:37 +0300

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,45 +3,38 @@
 """
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
     """
-    Defines a basic caching class that stores information in key-value pairs.
+    Defines a class for caching information in key-value pairs
+    Methods:
+        put(key, item) - store a key-value pair
+        get(key) - retrieve the value associated with a key
     """
 
     def __init__(self):
         """
-        Initializes the BasicCache class by calling the parent class's __init__ method.
+        Initialize the class using the parent class __init__ method
         """
         BaseCaching.__init__(self)
 
     def put(self, key, item):
         """
-        Stores a key-value pair in the cache.
-
+        Store a key-value pair
         Args:
-            key: The key to store.
-            item: The value associated with the key.
+            Key
+            Item
         """
         if key is None or item is None:
-            # If key or item is None, do nothing.
-            return
+            pass
         else:
-            # Add the key-value pair to the cache_data dictionary.
             self.cache_data[key] = item
 
     def get(self, key):
         """
-        Retrieves the value associated with a given key from the cache.
-
-        Args:
-            key: The key to retrieve the value for.
-
-        Returns:
-            The value associated with the key, or None if the key is None or doesn't exist.
+        Return value linked to key.
+        If key is None or doesn't exist, return None
         """
         if key is not None and key in self.cache_data.keys():
-            # If the key exists in the cache_data dictionary, return the corresponding value.
             return self.cache_data[key]
-        else:
-            # If the key is None or doesn't exist, return None.
-            return None
+        return None
