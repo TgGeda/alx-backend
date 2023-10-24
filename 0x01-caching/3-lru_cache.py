ccf7b0e49904b065601809a32c20bf881060e8b8
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-10-24 23:32:33 +0300

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -6,56 +6,38 @@ from base_caching import BaseCaching
 
 class LRUCache(BaseCaching):
     """
-    LRUCache defines a Least Recently Used (LRU) caching system.
+    FIFOCache defines a FIFO caching system
     """
     def __init__(self):
         """
-        Initializes the LRUCache class by calling the parent class's __init__ method.
-        It also initializes an empty list to keep track of the usage order of keys in the cache.
+        Initialize the class with the parent's init method
         """
         super().__init__()
         self.usage = []
 
     def put(self, key, item):
         """
-        Caches a key-value pair in the cache.
-
-        Args:
-            key: The key to cache.
-            item: The value associated with the key.
+        Cache a key-value pair
         """
         if key is None or item is None:
-            # If either key or item is None, do nothing.
-            return
+            pass
         else:
-            cache_size = len(self.cache_data)
-            if cache_size >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                # If the cache is full and the new key is not already in the cache, remove the least recently used item.
-                least_recently_used_key = self.usage[0]
-                print("DISCARD: {}".format(least_recently_used_key))
-                del self.cache_data[least_recently_used_key]
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.usage[0]))
+                del self.cache_data[self.usage[0]]
                 del self.usage[0]
             if key in self.usage:
-                # If the key is already in the cache, remove it from the current position.
                 del self.usage[self.usage.index(key)]
             self.usage.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
         """
-        Retrieves the value associated with a given key from the cache.
-
-        Args:
-            key: The key to retrieve the value for.
-
-        Returns:
-            The value associated with the key, or None if the key is None or doesn't exist in the cache.
+        Return the value linked to a given key, or None
         """
-        if key is not None and key in self.cache_data:
-            # If the key exists in the cache_data dictionary, update its usage position and return the corresponding value.
+        if key is not None and key in self.cache_data.keys():
             del self.usage[self.usage.index(key)]
             self.usage.append(key)
             return self.cache_data[key]
-        else:
-            # If the key is None or doesn't exist in the cache, return None.
-            return None
+        return None
